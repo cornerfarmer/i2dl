7b387d7f8d785f9fbb9a09abf99e20bbab3f444c
domin1101 <dominik.winkelbauer@outlook.de>
2018-06-27 19:39:58 +0200

    Adds ex4

---
+++ exercise_4/exercise_code/segmentation_nn.py
@@ -0,0 +1,58 @@
+"""SegmentationNN"""
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class SegmentationNN(nn.Module):
+
+    def __init__(self, num_classes=23):
+        super(SegmentationNN, self).__init__()
+
+        ########################################################################
+        #                             YOUR CODE                                #
+        ########################################################################
+
+        self.model_ft = models.vgg16(pretrained=True)
+        #self.model_ft.fc = nn.Linear(self.model_ft.fc.in_features, num_classes)
+
+        ########################################################################
+        #                             END OF YOUR CODE                         #
+        ########################################################################
+
+    def forward(self, x):
+        """
+        Forward pass of the convolutional neural network. Should not be called
+        manually but by calling a model instance directly.
+
+        Inputs:
+        - x: PyTorch input Variable
+        """
+        ########################################################################
+        #                             YOUR CODE                                #
+        ########################################################################
+
+        x = self.model_ft.forward(x)
+
+        ########################################################################
+        #                             END OF YOUR CODE                         #
+        ########################################################################
+
+        return x
+
+    @property
+    def is_cuda(self):
+        """
+        Check if model parameters are allocated on the GPU.
+        """
+        return next(self.parameters()).is_cuda
+
+    def save(self, path):
+        """
+        Save model with its parameters to the given path. Conventionally the
+        path should end with "*.model".
+
+        Inputs:
+        - path: path string
+        """
+        print('Saving model... %s' % path)
+        torch.save(self, path)
